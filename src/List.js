739deab63495a8a32306368346a89993e5edaf7c
Al Zimmerman <auraelius@gmail.com>
2019-05-10 19:25:13 -0700

    Card component wired up to verify prop handling.

--- src/List.js
+++ src/List.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Card from './Card.js';
 
 function List(props){
-  console.log(props);
+  console.log("List: props = ",props);
+  const theCards = props.cards.map((card)=>{
+    const newCard = {title: card.title, content: card.content};
+    console.log("List: new card = ",newCard);
+    return(
+      <Card title={card.title} content={card.content}/>
+    );
+  });
+
+  console.log("List: theCards = ",theCards);
   return(
   <div>
-    <p>{props.header}</p>
+    <p>({props.header}</p>
+    {theCards}
   </div>
   
     );
